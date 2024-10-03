64426c6efb5f12aa96e9449eecd8820dbe3523eb
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-03 11:30:32 +0100

    added : set the dto for the request.

--- src/main/java/net/axel/wafasalaf/models/dtos/RequestDto.java
+++ src/main/java/net/axel/wafasalaf/models/dtos/RequestDto.java
@@ -1,4 +1,22 @@
 package net.axel.wafasalaf.models.dtos;
 
-public record RequestDto() {
+import net.axel.wafasalaf.models.enums.Civility;
+
+import java.time.LocalDate;
+
+public record RequestDto(
+        String project,
+        String work,
+        double amountLoan,
+        int duration,
+        double monthly,
+        String phone,
+        Civility civility,
+        String lastName,
+        String firstName,
+        LocalDate birthday,
+        LocalDate hiringDay,
+        double income,
+        boolean haveCredit
+) {
 }

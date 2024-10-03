852b8d96c1d4082a1ee780fb3eb8a5cc0b23be77
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-03 14:37:01 +0100

    added : set the saveRequest(), where we create ou object from the dto object that we get from the UI.

---
+++ src/main/java/net/axel/wafasalaf/services/implementations/RequestService.java
@@ -0,0 +1,58 @@
+package net.axel.wafasalaf.services.implementations;
+
+import net.axel.wafasalaf.models.dtos.RequestDto;
+import net.axel.wafasalaf.models.entities.Request;
+import net.axel.wafasalaf.repositories.implementations.RequestRepository;
+import net.axel.wafasalaf.repositories.interfaces.IRequestRepository;
+import net.axel.wafasalaf.services.interfaces.IRequestService;
+
+import java.util.List;
+import java.util.UUID;
+
+public class RequestService implements IRequestService {
+    private final IRequestRepository requestRepository;
+
+    public RequestService(RequestRepository requestRepository) {
+        this.requestRepository = requestRepository;
+    }
+
+    @Override
+    public Request saveRequest(RequestDto requestDto) {
+        Request request = new Request();
+        request.setProject(requestDto.project());
+        request.setWork(requestDto.work());
+        request.setAmountLoan(requestDto.amountLoan());
+        request.setDuration(requestDto.duration());
+        request.setMonthly(requestDto.monthly());
+        request.setPhone(requestDto.phone());
+        request.setCivility(requestDto.civility());
+        request.setLast_name(requestDto.lastName());
+        request.setFirst_name(requestDto.firstName());
+        request.setBirthday(requestDto.birthday());
+        request.setHiringDay(requestDto.hiringDay());
+        request.setIncome(requestDto.income());
+        request.setHaveCredit(requestDto.haveCredit());
+
+        return requestRepository.save(request);
+    }
+
+    @Override
+    public Request findRequestById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public List<Request> findAllRequests() {
+        return List.of();
+    }
+
+    @Override
+    public Request updateRequest(String oldName, RequestDto updatedRequest) {
+        return null;
+    }
+
+    @Override
+    public void deleteRequest(String name) {
+
+    }
+}

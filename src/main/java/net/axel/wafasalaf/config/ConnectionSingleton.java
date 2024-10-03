af985179f58f58afd022b504ee1a18b96149c5d2
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-03 09:58:37 +0100

    set the ENTITY_MANAGER_FACTORY to become final.

--- src/main/java/net/axel/wafasalaf/config/ConnectionSingleton.java
+++ src/main/java/net/axel/wafasalaf/config/ConnectionSingleton.java
@@ -4,7 +4,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class ConnectionSingleton {
-    private static EntityManagerFactory ENTITY_MANAGER_FACTORY;
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY;
 
     static {
         ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("WAFA_SALAF");

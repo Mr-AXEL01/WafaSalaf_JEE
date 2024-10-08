0d4b9d24170423b3b0f4c57a9b02f7bb49e0b6ce
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-08 17:07:36 +0100

    added : create a private method to recalcul the monthly to not enter a wrong infos to our DB. and set a ConstraintViolation for the validation of the DTO .

--- src/main/java/net/axel/wafasalaf/services/implementations/RequestService.java
+++ src/main/java/net/axel/wafasalaf/services/implementations/RequestService.java
@@ -1,5 +1,10 @@
 package net.axel.wafasalaf.services.implementations;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.ValidationException;
+import jakarta.validation.Validator;
+import net.axel.wafasalaf.exception.ValidationRequestException;
 import net.axel.wafasalaf.models.dtos.RequestDto;
 import net.axel.wafasalaf.models.entities.Request;
 import net.axel.wafasalaf.repositories.implementations.RequestRepository;
@@ -7,32 +12,44 @@ import net.axel.wafasalaf.repositories.interfaces.IRequestRepository;
 import net.axel.wafasalaf.services.interfaces.IRequestService;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class RequestService implements IRequestService {
     private final IRequestRepository requestRepository;
+    private final Validator validator;
 
     public RequestService(RequestRepository requestRepository) {
         this.requestRepository = requestRepository;
+        this.validator = Validation.buildDefaultValidatorFactory().getValidator();
     }
 
     @Override
-    public Request saveRequest(RequestDto requestDto) {
-        Request request = new Request();
-        request.setProject(requestDto.project());
-        request.setWork(requestDto.work());
-        request.setAmountLoan(requestDto.amountLoan());
-        request.setDuration(requestDto.duration());
-        request.setMonthly(requestDto.monthly());
-        request.setPhone(requestDto.phone());
-        request.setCivility(requestDto.civility());
-        request.setLast_name(requestDto.lastName());
-        request.setFirst_name(requestDto.firstName());
-        request.setBirthday(requestDto.birthday());
-        request.setHiringDay(requestDto.hiringDay());
-        request.setIncome(requestDto.income());
-        request.setHaveCredit(requestDto.haveCredit());
+    public Request saveRequest(RequestDto dto) {
+        Set<ConstraintViolation<RequestDto>> constraintViolations = validator.validate(dto);
 
+        if (!constraintViolations.isEmpty()) {
+            /*
+            key => field name
+            value => message
+             */
+            Map<String, String> errors = constraintViolations.stream()
+                    .collect(Collectors.toMap(
+                            c -> c.getPropertyPath().toString(),
+                            ConstraintViolation::getMessage
+                    ));
+            throw new ValidationRequestException(errors);
+        }
+
+//        check if the calculation is right
+        final double amount = dto.amountLoan();
+        final int duration = dto.duration();
+        Double monthly = checkCalcul(amount, duration);
+
+
+        Request request = new Request(dto.project(), dto.work(), dto.amountLoan(), dto.duration(), monthly, dto.email(), dto.phone(), dto.civility(), dto.lastName(), dto.firstName(), dto.cin(), dto.birthDate(), dto.hiringDate(), dto.income(), dto.haveCredit());
         return requestRepository.save(request);
     }
 
@@ -59,8 +76,8 @@ public class RequestService implements IRequestService {
         updatedRequest.setCivility(dto.civility());
         updatedRequest.setLast_name(dto.lastName());
         updatedRequest.setFirst_name(dto.firstName());
-        updatedRequest.setBirthday(dto.birthday());
-        updatedRequest.setHiringDay(dto.hiringDay());
+        updatedRequest.setBirthDate(dto.birthDate());
+        updatedRequest.setHiringDate(dto.hiringDate());
         updatedRequest.setIncome(dto.income());
         updatedRequest.setHaveCredit(dto.haveCredit());
         return requestRepository.update(updatedRequest);
@@ -69,5 +86,11 @@ public class RequestService implements IRequestService {
     @Override
     public void deleteRequest(UUID id) {
         requestRepository.delete(id);
+    }
+
+    private Double checkCalcul(double amount, int duration) {
+        final double interestRate = 0.012;
+        final double amountTotal = amount * ( 1 + interestRate);
+        return amountTotal / duration;
     }
 }

0b2b721d38d23f049dd3bb9e9d9cd1dd20931cc2
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-08 16:52:41 +0100

    added : validate the attribut of the dto and add cin to the dto.

--- src/main/java/net/axel/wafasalaf/models/dtos/RequestDto.java
+++ src/main/java/net/axel/wafasalaf/models/dtos/RequestDto.java
@@ -1,22 +1,27 @@
 package net.axel.wafasalaf.models.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import net.axel.wafasalaf.models.enums.Civility;
 
 import java.time.LocalDate;
 
 public record RequestDto(
-        String project,
-        String work,
-        double amountLoan,
-        int duration,
-        double monthly,
-        String phone,
-        Civility civility,
-        String lastName,
-        String firstName,
-        LocalDate birthday,
-        LocalDate hiringDay,
-        double income,
-        boolean haveCredit
+        @NotBlank String project,
+        @NotBlank String work,
+        @NotNull double amountLoan,
+        @NotNull int duration,
+        @NotNull double monthly,
+        @NotBlank @Email String email,
+        @NotBlank  String phone,
+        @NotNull Civility civility,
+        @NotBlank String lastName,
+        @NotBlank String firstName,
+        @NotBlank String cin,
+        @NotNull LocalDate birthDate,
+        @NotNull LocalDate hiringDate,
+        @NotNull double income,
+        @NotNull boolean haveCredit
 ) {
 }

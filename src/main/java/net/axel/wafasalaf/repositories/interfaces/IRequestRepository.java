ee629f9d6463abcc5d3bcd52a3e66e0aec001b44
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-10 11:21:50 +0100

    Refactor : remove the declaration of the methods signature and extend from the IJpaRepository that have the generic methods signatures

--- src/main/java/net/axel/wafasalaf/repositories/interfaces/IRequestRepository.java
+++ src/main/java/net/axel/wafasalaf/repositories/interfaces/IRequestRepository.java
@@ -2,18 +2,7 @@ package net.axel.wafasalaf.repositories.interfaces;
 
 import net.axel.wafasalaf.models.entities.Request;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
-public interface IRequestRepository {
-    Request save(Request request);
-
-    Optional<Request> findById(UUID id);
-
-    List<Request> findAll();
-
-    Request update(Request request);
-
-    void delete(UUID id);
+public interface IRequestRepository extends IJpaRepository<Request, UUID>{
 }

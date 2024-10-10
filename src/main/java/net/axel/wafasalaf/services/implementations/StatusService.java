ecf4e97799e77d12a9f941d96ab80ec47f6beb61
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-10 14:07:55 +0100

    Added : handle the save and the find of status

---
+++ src/main/java/net/axel/wafasalaf/services/implementations/StatusService.java
@@ -0,0 +1,30 @@
+package net.axel.wafasalaf.services.implementations;
+
+import net.axel.wafasalaf.models.dtos.StatusDto;
+import net.axel.wafasalaf.models.entities.Status;
+import net.axel.wafasalaf.repositories.implementations.StatusRepository;
+import net.axel.wafasalaf.repositories.interfaces.IStatusRepository;
+import net.axel.wafasalaf.services.interfaces.IStatusService;
+
+import java.util.UUID;
+
+public class StatusService implements IStatusService {
+
+    private final IStatusRepository statusRepository;
+
+    public StatusService(StatusRepository statusRepository) {
+        this.statusRepository = statusRepository;
+    }
+
+    @Override
+    public Status saveStatus(StatusDto dto) {
+        Status status = new Status(dto.name());
+        return statusRepository.save(status);
+    }
+
+    @Override
+    public Status findStatusById(UUID id) {
+        return statusRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Error getting status by ID : " + id));
+    }
+}

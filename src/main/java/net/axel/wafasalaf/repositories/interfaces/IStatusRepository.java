388fc74185a28d34782e621c17f57b44ae87f686
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-10 11:50:12 +0100

    Added : extend from the IJpaRepository that have the generic methods signatures

---
+++ src/main/java/net/axel/wafasalaf/repositories/interfaces/IStatusRepository.java
@@ -0,0 +1,8 @@
+package net.axel.wafasalaf.repositories.interfaces;
+
+import net.axel.wafasalaf.models.entities.Status;
+
+import java.util.UUID;
+
+public interface IStatusRepository extends IJpaRepository<Status, UUID>{
+}

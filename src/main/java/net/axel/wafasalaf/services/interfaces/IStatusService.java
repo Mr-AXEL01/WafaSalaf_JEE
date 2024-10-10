123c8fc939bdc46817d53c3bd071b52a42c777ee
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-10 14:07:11 +0100

    Added : create the inerface for statuse service and declare the methods signature for save and find status

---
+++ src/main/java/net/axel/wafasalaf/services/interfaces/IStatusService.java
@@ -0,0 +1,13 @@
+package net.axel.wafasalaf.services.interfaces;
+
+import net.axel.wafasalaf.models.dtos.StatusDto;
+import net.axel.wafasalaf.models.entities.Status;
+
+import java.util.UUID;
+
+public interface IStatusService {
+    Status saveStatus(StatusDto dto);
+
+    Status findStatusById(UUID id);
+
+}

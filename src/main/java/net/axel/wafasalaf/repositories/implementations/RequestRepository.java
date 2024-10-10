ff4189a7d05dcfdf433cc9e2a696a4814f777fd5
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-10 11:19:15 +0100

    Refactor : extend from the JpaRepository that have our generics operations . and create a constructor to pass the entityType => Request.class .

--- src/main/java/net/axel/wafasalaf/repositories/implementations/RequestRepository.java
+++ src/main/java/net/axel/wafasalaf/repositories/implementations/RequestRepository.java
@@ -1,103 +1,12 @@
 package net.axel.wafasalaf.repositories.implementations;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
-import net.axel.wafasalaf.config.ConnectionSingleton;
 import net.axel.wafasalaf.models.entities.Request;
 import net.axel.wafasalaf.repositories.interfaces.IRequestRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
-public class RequestRepository implements IRequestRepository {
-
-    private final EntityManager entityManager;
-    private final EntityTransaction transaction;
-
+public class RequestRepository extends JpaRepository<Request, UUID> implements IRequestRepository {
     public RequestRepository() {
-        this.entityManager = ConnectionSingleton.entityManagerFactory().createEntityManager();
-        this.transaction = entityManager.getTransaction();
-    }
-
-    @Override
-    public Request save(Request request) {
-        try {
-            transaction.begin();
-            entityManager.persist(request);
-            transaction.commit();
-        } catch(RuntimeException e) {
-            transaction.rollback();
-            e.printStackTrace();
-        } finally {
-            entityManager.close();
-        }
-        return request;
-    }
-
-    @Override
-    public Optional<Request> findById(UUID id) {
-        Request request = null;
-        try {
-            transaction.begin();
-            request = entityManager.find(Request.class, id);
-            transaction.commit();
-        } catch (RuntimeException e) {
-            transaction.rollback();
-            e.printStackTrace();
-        } finally {
-            entityManager.close();
-        }
-        return Optional.ofNullable(request);
-    }
-
-    @Override
-    public List<Request> findAll() {
-        List<Request> requests = new ArrayList<>();
-        try {
-            transaction.begin();
-            requests = entityManager.createQuery("SELECT r FROM Request r", Request.class).getResultList();
-            transaction.commit();
-        } catch (RuntimeException e) {
-            transaction.rollback();
-            e.printStackTrace();
-        } finally {
-            entityManager.close();
-        }
-        return requests;
-    }
-
-    @Override
-    public Request update(Request request) {
-        Request updatedRequest = null;
-        try {
-            transaction.begin();
-            updatedRequest = entityManager.merge(request);
-            transaction.commit();
-        } catch (RuntimeException e) {
-            transaction.rollback();
-            e.printStackTrace();
-        } finally {
-            entityManager.close();
-        }
-        return updatedRequest;
-    }
-
-    @Override
-    public void delete(UUID id) {
-        try {
-            transaction.begin();
-            Request request = entityManager.find(Request.class, id);
-            if (request != null) {
-            entityManager.remove(request);
-            }
-            transaction.commit();
-        } catch (RuntimeException e) {
-            transaction.rollback();
-            e.printStackTrace();
-        } finally {
-            entityManager.close();
-        }
+        super(Request.class);
     }
 }

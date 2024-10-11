bed783f26a976f1e4c2d399c179cda335bff310f
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-11 16:47:03 +0100

    Refactor : update code source to instance an entity manager and a transaction for each action.

--- src/main/java/net/axel/wafasalaf/repositories/implementations/JpaRepository.java
+++ src/main/java/net/axel/wafasalaf/repositories/implementations/JpaRepository.java
@@ -1,6 +1,7 @@
 package net.axel.wafasalaf.repositories.implementations;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import net.axel.wafasalaf.config.ConnectionSingleton;
 import net.axel.wafasalaf.repositories.interfaces.IJpaRepository;
@@ -11,18 +12,17 @@ import java.util.Optional;
 
 public class JpaRepository<T, ID> implements IJpaRepository<T, ID> {
 
-    private final EntityManager entityManager;
-    private final EntityTransaction transaction;
+
     private final Class<T> entityType;
 
     public JpaRepository(Class<T> entityType) {
-        this.entityManager = ConnectionSingleton.entityManagerFactory().createEntityManager();
-        this.transaction = entityManager.getTransaction();
         this.entityType = entityType;
     }
 
     @Override
     public T save(T entity) {
+        EntityManager entityManager = ConnectionSingleton.getEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
         try {
             transaction.begin();
             entityManager.persist(entity);
@@ -38,6 +38,8 @@ public class JpaRepository<T, ID> implements IJpaRepository<T, ID> {
 
     @Override
     public Optional<T> findById(ID id) {
+        EntityManager entityManager = ConnectionSingleton.getEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
         T entity = null;
         try {
             transaction.begin();
@@ -54,6 +56,8 @@ public class JpaRepository<T, ID> implements IJpaRepository<T, ID> {
 
     @Override
     public List<T> findAll() {
+        EntityManager entityManager = ConnectionSingleton.getEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
         List<T> entities = new ArrayList<>();
         try {
             transaction.begin();
@@ -70,6 +74,8 @@ public class JpaRepository<T, ID> implements IJpaRepository<T, ID> {
 
     @Override
     public T update(T entity) {
+        EntityManager entityManager = ConnectionSingleton.getEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
         T updatedEntity = null;
         try {
             transaction.begin();
@@ -86,6 +92,8 @@ public class JpaRepository<T, ID> implements IJpaRepository<T, ID> {
 
     @Override
     public void delete(ID id) {
+        EntityManager entityManager = ConnectionSingleton.getEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
         try {
             transaction.begin();
             T entity = entityManager.find(entityType, id);

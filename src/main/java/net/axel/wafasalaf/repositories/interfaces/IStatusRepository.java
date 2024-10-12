fe3edf4741440168ffed6ba0dc8041c64a73eba4
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-12 15:44:47 +0100

    added : declare the method signature to find a status by name.

--- src/main/java/net/axel/wafasalaf/repositories/interfaces/IStatusRepository.java
+++ src/main/java/net/axel/wafasalaf/repositories/interfaces/IStatusRepository.java
@@ -2,7 +2,9 @@ package net.axel.wafasalaf.repositories.interfaces;
 
 import net.axel.wafasalaf.models.entities.Status;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface IStatusRepository extends IJpaRepository<Status, UUID>{
+    Optional<Status> findByName(String name);
 }

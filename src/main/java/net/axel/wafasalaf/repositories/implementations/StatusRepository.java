9e564d9d1c18bae1dfb6c0eb91fdecf58c1b11b8
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-10-13 14:53:23 +0100

    Refactor : update the method findByName() to use createQuery insted of using find , cause find is for primary key.

--- src/main/java/net/axel/wafasalaf/repositories/implementations/StatusRepository.java
+++ src/main/java/net/axel/wafasalaf/repositories/implementations/StatusRepository.java
@@ -2,6 +2,7 @@ package net.axel.wafasalaf.repositories.implementations;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.NoResultException;
 import net.axel.wafasalaf.config.ConnectionSingleton;
 import net.axel.wafasalaf.models.entities.Status;
 import net.axel.wafasalaf.repositories.interfaces.IStatusRepository;
@@ -14,14 +15,21 @@ public class StatusRepository extends JpaRepository<Status, UUID> implements ISt
         super(Status.class);
     }
 
+    @Override
     public Optional<Status> findByName(String name) {
         EntityManager entityManager = ConnectionSingleton.getEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
-        Status status= null;
+        Status status = null;
+
         try {
             transaction.begin();
-            status = entityManager.find(Status.class, name);
+            status = entityManager
+                    .createQuery("SELECT s FROM Status s WHERE s.name = :name", Status.class)
+                    .setParameter("name", name)
+                    .getSingleResult();
             transaction.commit();
+        } catch (NoResultException e) {
+            transaction.rollback();
         } catch (RuntimeException e) {
             transaction.rollback();
             e.printStackTrace();
